10febf36958d5cebbc101f84426663a1272c7cce
szymonborkowski <szymonborkowski10@gmail.com>
2025-12-14 10:04:02 +0000

    improvements on saving to SD

--- sd_card_datalogging/sd_card_datalogging.ino
+++ sd_card_datalogging/sd_card_datalogging.ino
@@ -9,14 +9,13 @@ Logging data from a GPS module using SD card and Arduino
 // SD Card Shield:
 const int chipSelect = 4;  // CS for Arduino Uno Rev3e + seeed studio SD Card Shield v4.4
 File coordinateLogs;
+volatile unsigned int counter = 0;
 
 // GPS: 
 SoftwareSerial gpsSerial(8,7);  // Change to hardware serial when I swap to Mega board
 #define PMTK_SET_NMEA_UPDATE_1HZ  "$PMTK220,1000*1F"
 #define PMTK_SET_NMEA_OUTPUT_ALLDATA "$PMTK314,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0*28"
 
-// Temp counter:
-volatile unsigned int counter = 0;
 
 void setup() {
   while(!Serial);
@@ -32,11 +31,11 @@ void setup() {
   }
   Serial.println("init done.");
 
-  // Open a new file and write data
+  // Open file and overwrite data if exists
   Serial.println("creating file");
-  coordinateLogs = SD.open("coords.txt", FILE_WRITE);  // limited by 8.3 filename format  
+  coordinateLogs = SD.open("coords_F.txt", O_WRITE | O_CREAT | O_TRUNC);  // limited by 8.3 filename format  
 
-  Serial.println("test...");
+  Serial.println("init gps");
   gpsSerial.println(PMTK_SET_NMEA_OUTPUT_ALLDATA);
   gpsSerial.println(PMTK_SET_NMEA_UPDATE_1HZ);
 
@@ -47,17 +46,19 @@ void loop() {
     char c = gpsSerial.read();  // Read stream from GPS
     Serial.write(c);
 
+    counter++;
+
     // If the file opened okay, write to it:
     if (coordinateLogs) {
-      coordinateLogs.println(c);
+      coordinateLogs.print(c);
       
-      // Close the file after 100 characters:
-      if (counter == 100) {
-        coordinateLogs.close();
+      // Every 1000 chars save the file
+      if (counter % 1000 == 0) {
+        coordinateLogs.flush();
       }
     } else {
       // File reached 100 characters
-      Serial.println("closed file");
+      Serial.println("error opening file");
     }
   }
 }
